5e41f86ecd340c72bddfecb03e426f14d8b4042e
egrash <135422335+egrash@users.noreply.github.com>
2023-06-04 11:09:34 +0000

    strings

---
+++ 1.строки/task_1.py
@@ -0,0 +1,7 @@
+car = "ferrari"
+name = "Egor"
+
+# напиши чтобы вывелось Egor have ferrari: 
+print(name, "have", car)
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+print("HELLO!!!") 
+print('HEELLLLOOOOO')
+
+print(5+1111)
+print(5.4 + 10.6)
+
+
+# string (str)
+# "string" 
+# 'string'
+
+# 5 # integer (int)
+# 5.1 # float (число в плавающей точкой)
+
+# bool 
+# True
+# False
+
+# # list (список)
+# ['str', 'ing', 324, 5.4]
+
+# # tuple 
+# (5, 5, 6, 7, 8, 8, "dfsdf")
+
+# # set
+# {5, 5, 6, 7, 8, 8, "dfsdf"}
+
+# dict (словарь)
+bro = {"Egor": "Daniil", "Lesha": "Lera"}
+# print(bro)
+# print(bro["Egor"])
+# print(bro["Lesha"])
+
+
+for name in bro:
+    print(name, " у меня брат или сестра - это ", bro[name])
+for name in bro:
+    print(name,'у меня брат или сестра - это' ,bro[name])
